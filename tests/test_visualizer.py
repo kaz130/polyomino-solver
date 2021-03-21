51b55a6fc7918a474927dc95a28f53a1c0ae62ae
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-03-21 17:39:11 +0900

    Add test visualizer

---
+++ tests/test_visualizer.py
@@ -0,0 +1,31 @@
+import pytest
+
+from cpsolver.board import Board
+from cpsolver.piece import Piece
+from cpsolver.visualizer import Visualizer
+
+board_str = '''
+###
+###'''.split("\n")[1:]
+
+piece_str = '''
+###
+##'''.split("\n")[1:]
+
+expectation = '''\
+┏━━━━━┓
+┃   ┏━┛
+┗━━━┛  '''
+
+board = Board(board_str)
+piece = Piece(piece_str)
+q_values = [
+        [[[0, 0, 0, 1, 0, 0, 0, 0]], [[0, 0, 0, 0, 0, 0, 0, 0]], [[0, 0, 0, 0, 0, 0, 0, 0]]],
+        [[[0, 0, 0, 0, 0, 0, 0, 0]], [[0, 0, 0, 0, 0, 0, 0, 0]], [[0, 0, 0, 0, 0, 0, 0, 0]]]]
+
+def test_visualize():
+    r = '\n'.join(map(lambda x: ''.join(x), Visualizer().make_str([piece], board, q_values)))
+    print(r)
+    print(expectation)
+    assert r == expectation
+
